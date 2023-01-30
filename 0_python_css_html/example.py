6b8b5b7ed2354b564f879555354673d0dc464539
camilozg <camilozamora1@gmail.com>
2023-01-29 20:04:44 -0500

    carpeta python,css y html agregada

---
+++ 0_python_css_html/example.py
@@ -0,0 +1,22 @@
+myvar = "Hello Mike"
+
+# Imprimir la ultima letra
+print(myvar[-1])
+
+print(myvar[0:1])
+print(myvar[0:])
+print(myvar[1:-1])
+
+print(myvar.split(" "))
+
+first_name = "Camilo"
+second_name = "Zamora"
+
+print(f"Hello my name is {first_name} {second_name}")
+
+text = 'EPIC'
+
+print(f'{text}')
+print(f'{text:#<20}')
+print(f'{text:_>20}')
+print(f'{text:.^20}')
